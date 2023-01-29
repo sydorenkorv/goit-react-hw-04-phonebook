1a1528bc697e9a7dc02901d7987aa3275b5ba1f9
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-01-29 16:43:55 +0000

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -17,7 +17,7 @@ export const ContactForm = ({
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+        title="Name  may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
         required
         value={newContactName}
         onChange={event => setNewContactName(event.target.value)}
